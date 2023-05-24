52e3a4483a0488463e14b712105e796116095a3d
Gerald Odoh <contactodohgerald@gmail>
2023-05-24 14:40:09 +0100

    added a new vercel toast and still didn't work, trying smth new

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import "vercel-toast/css";
-import 'react-toastify/dist/ReactToastify.min.css';
 import { ToastContainer } from 'react-toastify';
 
 const Layout = ({ children }) => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/tailwind.css";
 import "../styles/slick.css";
 
+import "vercel-toast/css";
+import 'react-toastify/dist/ReactToastify.min.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
